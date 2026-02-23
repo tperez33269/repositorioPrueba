e3fd1f4adcc1ea18956a2afb48c0bc6a8f188083
tperez33269 <tperez33269@alumnos.uemc.es>
2026-02-23 15:32:19 +0100

    fix: char to int

--- suma.c
+++ suma.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int suma(int a, int b){
 	return a+b;
-
+}
 
 int main(int argc, char** argv){
-	int resultado = suma(argv[1], argv[2]);
+	int resultado = suma(atoi(argv[1]),atoi( argv[2]));
+	
+	printf("%d\n", resultado);
+	
 	return 0;
 }
 	
